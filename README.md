b0be7c16f518002688b97aa808c38129a5fb3e2b
Rohit <singhrohit51967@gmail.com>
2024-04-04 08:38:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ML_Lab_Project
+
+# pip install --upgrade imbalanced-learn
+# pip install --upgrade scikit-learn
+# pip install flask
+# pip install gunicorn
+
+# python app.py
---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def man():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    arr = []
+    for i in range(1, 28):
+        key = 'a' + str(i)
+        data = request.form.get(key)
+        arr.append(data)
+    arr = np.array([arr])
+    pred = model.predict(arr)
+    return render_template('after.html', data=pred)
+
+if __name__ == "__main__":
+    app.run(debug=True)
